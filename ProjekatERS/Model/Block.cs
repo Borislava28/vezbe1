b806c1a89586fef0d1f0340387834fb57398b591
Borislava28 <148521698+Borislava28@users.noreply.github.com>
2023-12-20 18:52:27 +0100

    Update class Block

--- ProjekatERS/Model/Block.cs
+++ ProjekatERS/Model/Block.cs
@@ -8,5 +8,21 @@ namespace ProjekatERS.Model
 {
     public class Block
     {
+        public Guid Id { get; set; }
+
+        public double num { get; set; }
+        public Guid? PreviousBlockId { get; set; }
+
+        public Podatak Vrednost { get; set; }
+
+        public DateTime Datum { get; set; }
+
+        public Block(Podatak vrednost)
+        {
+            Id = Guid.NewGuid();
+            Vrednost = vrednost;
+            num = 0.00001;
+            PreviousBlockId = null;
+        }
     }
 }
